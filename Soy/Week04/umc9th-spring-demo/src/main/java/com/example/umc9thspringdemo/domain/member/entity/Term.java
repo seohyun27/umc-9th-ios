c5b6b4da85d77368499be7d831d9948740e6aa30
seoyeoki <kimseoyeon0724@yu.ac.kr>
2025-10-11 18:55:48 +0900

    feat: Member/SnsLogin, Food 엔티티 추가, Member, Term 엔티티 변경

---
+++ Soy/Week04/umc9th-spring-demo/src/main/java/com/example/umc9thspringdemo/domain/member/entity/Term.java
@@ -0,0 +1,30 @@
+package com.example.umc9thspringdemo.domain.member.entity;
+
+import com.example.umc9thspringdemo.domain.member.enums.TermName;
+import com.example.umc9thspringdemo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "term")
+public class Term extends BaseEntity {
+
+    @Id
+    @Column(name = "term_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    @Enumerated(EnumType.STRING)
+    private TermName termName;
+
+    @Column(name = "desc")
+    private String desc;
+
+    @Column(name = "status")
+    private boolean isValid;
+}
