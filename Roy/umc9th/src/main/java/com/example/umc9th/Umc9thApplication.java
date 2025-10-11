ade98813e94db3c1408c516cb2c367aadd4f717b
yujin <yujin09219@naver.com>
2025-10-11 14:37:05 +0900

    fix: Application 경로 수정

--- Roy/umc9th/src/main/java/com/example/umc9th/Umc9thApplication.java
+++ Roy/umc9th/src/main/java/com/example/umc9th/Umc9thApplication.java
@@ -1,14 +1,11 @@
-package com.example.umc9th.global;
+package com.example.umc9th;
 
-import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.umc9th") // Controller, Service 등
 @EntityScan(basePackages = "com.example.umc9th.domain") // Entity
 public class Umc9thApplication {
 
