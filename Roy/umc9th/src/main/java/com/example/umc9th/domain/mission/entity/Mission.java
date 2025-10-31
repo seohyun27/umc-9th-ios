da5a487446a412a0e28142b18b1d5087f3a3b6c6
yujin <yujin09219@naver.com>
2025-10-31 15:57:00 +0900

    refactor : 필드 수정

--- Roy/umc9th/src/main/java/com/example/umc9th/domain/mission/entity/Mission.java
+++ Roy/umc9th/src/main/java/com/example/umc9th/domain/mission/entity/Mission.java
@@ -1,6 +1,7 @@
 package com.example.umc9th.domain.mission.entity;
 
 import com.example.umc9th.domain.store.entity.Store;
+import com.example.umc9th.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,7 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "mission")
-public class Mission {
+public class Mission extends BaseEntity {
 
   @Id
   @Column(name = "mission_id")
